e368f1bc58d95cbfb6551cd9c91bea66690e4249
samurai-98 <link6234@gmail.com>
2023-04-28 11:00:22 -0600

    First commit

---
+++ ssl_replace.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "File location: "
+read directory
+
+echo "Key file name: "
+read keyFile
+
+echo "Certificate file name: "
+read certFile
+
+cd /etc/vmware/ssl
+pwd
+
+echo "Backing up former certificate and key"
+cp rui.crt rui.crt.bak
+cp rui.key rui.key.bak
+echo "Done"
+
+echo "Copying new certificate and key"
+cp /$directory/$certFile rui.crt
+cp /$directory/$keyFile rui.key
+echo "Done"
