5da79354ce59b9d325f2a1d2e5ed9a40cdb12b17
samurai-98 <link6234@gmail.com>
2023-05-02 13:04:40 -0600

    Added SCP and SSH functions

--- ssl_replace.sh
+++ ssl_replace.sh
@@ -1,25 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-# This script automates the process for replacing SSL certificates and keys on ESXi hosts
+#This script automates the replacement/renewal of SSL certificates on ESXi hosts
 
-echo "File location: "
-read directory
-
+echo "Host name: "
+read hostName
 echo "Key file name: "
 read keyFile
-
 echo "Certificate file name: "
 read certFile
-
+scp -P 22 $certFile $keyFile root@$hostName:~/tmp/
+ssh -tt root@$hostName << EOF
 cd /etc/vmware/ssl
-pwd
-
-echo "Backing up former certificate and key"
 cp rui.crt rui.crt.bak
 cp rui.key rui.key.bak
-echo "Done"
-
-echo "Copying new certificate and key"
-cp /$directory/$certFile rui.crt
-cp /$directory/$keyFile rui.key
+cp /tmp/$certFile rui.crt
+yes
+cp /tmp/$keyFile rui.key
+yes
+exit
+EOF
 echo "Done"
